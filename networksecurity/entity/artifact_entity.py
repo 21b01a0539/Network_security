c803eb22a38379b23d4ebabecc4a6d30b593512e
21b01a0539 <djbarghavi.dasari@gmail.com>
2024-08-24 11:40:18 +0530

    config and artifect structure updated

---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataValidationArtifact:
+    pass
+
+@dataclass
+class DataTransformationArtifact:
+    pass
+
+@dataclass
+class ModelTrainerArtifact:
+    pass
+
+@dataclass
+class ModelEvaluationArtifact:
+    pass
+
+@dataclass
+class ModelPusherArtifact:
+    pass
