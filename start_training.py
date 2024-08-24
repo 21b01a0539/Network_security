2c15d853a1364ee997a2993ace7a6f16de0f72f1
21b01a0539 <djbarghavi.dasari@gmail.com>
2024-08-24 13:00:29 +0530

    Folder updated

--- start_training.py
+++ start_training.py
@@ -9,9 +9,12 @@ from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 def start_training():
     try:
-        pass
+        logging.info("training has started")
+
+        model_training=TrainingPipeline()
+        model_training.run_pipeline()
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
 if __name__=='__main__':
-    pass
+    start_training()
