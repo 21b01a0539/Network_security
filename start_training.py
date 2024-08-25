8530143e6c120b455325a18de8b59697b36ddfd1
21b01a0539 <djbarghavi.dasari@gmail.com>
2024-08-25 10:30:34 +0530

    training pipeline testing

--- start_training.py
+++ start_training.py
@@ -10,10 +10,11 @@ from networksecurity.pipeline.training_pipeline import TrainingPipeline
 def start_training():
     try:
         logging.info("training has started")
-
-        model_training=TrainingPipeline()
-        model_training.run_pipeline()
+        print("Hi")
+        # model_training=TrainingPipeline()
+        # model_training.run_pipeline()
     except Exception as e:
+        print("hey error")
         raise NetworkSecurityException(e,sys)
     
 if __name__=='__main__':
