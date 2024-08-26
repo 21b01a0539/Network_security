3de43684da25a8df462a8e5cb367469a254a5a85
21b01a0539 <djbarghavi.dasari@gmail.com>
2024-08-26 16:43:50 +0530

    model trainer code is ready

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -59,9 +59,21 @@ DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
 """Model Trainer related constants MODEL_TRAINER VAR NAME
 """
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
 
 """Model Evaluation related constants MODEL_EVALUATION VAR NAME
 """
+MODEL_EVALUATION_DIR_NAME: str = "model_evaluation"
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_EVALUATION_REPORT_NAME= "report.yaml"
+
 
 """Model Pusher related constants MODEL_PUSHER VAR NAME 
 """
+MODEL_PUSHER_DIR_NAME = "model_pusher"
+MODEL_PUSHER_SAVED_MODEL_DIR = SAVED_MODEL_DIR
