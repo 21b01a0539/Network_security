27b61cdf9fbd9fee7922694f269661e4edcb4f40
21b01a0539 <djbarghavi.dasari@gmail.com>
2024-09-06 15:21:25 +0530

    modules updated

--- start_training.py
+++ start_training.py
@@ -13,7 +13,6 @@ def start_training():
         model_training=TrainingPipeline()
         model_training.run_pipeline()
     except Exception as e:
-        print("hey error")
         raise NetworkSecurityException(e,sys)
     
 if __name__=='__main__':
